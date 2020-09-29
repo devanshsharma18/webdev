aaf942e0ab82eaa8446ac22dfbaeff9c99f3e154
devanshsharma18 <42463930+devanshsharma18@users.noreply.github.com>
2020-09-29 11:35:11 +0530

    Add files via upload

---
+++ studentinfo.php
@@ -0,0 +1,36 @@
+<html>
+   <form action="Result.php">
+      <table>
+         <caption>STUDENT MARKSHEET FORM</caption>
+         <tr>
+            <td>Student Name</td>
+            <td><input type=text name=sn size=30></td>
+        </tr>
+         <tr>
+            <td>Dob</td>
+            <td><input type=text name=dob size=30></td>
+         </tr>
+         <tr>
+            <td>Gender</td>
+            <td><input type=radio name=gen size=30 value="Male">Male<input type=radio name=gen size=30 value="Female">Female</td>
+         </tr>
+         <tr>
+            <td>Roll no.</td>
+            <td><input type=text name=rn size=30></td>
+         </tr>
+         <tr>
+            <td>English marks</td>
+            <td><input type=text name=eng size=30></td>
+         </tr>
+         <tr>
+            <td>Maths marks</td>
+            <td><input type=text name=math size=30></td>
+         </tr>
+        
+         <tr>
+            <td><input type=submit></td>
+            <td><input type=reset></td>
+         </tr>
+      </table>
+   </form>
+</html>
